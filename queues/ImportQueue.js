85906a0882e1bc79316d9bb7ec9a53dc14a7cc04
felix <felixxplore08@gmail.com>
2025-10-28 20:17:34 +0530

    first commit

--- queues/ImportQueue.js
+++
@@ -1,9 +0,0 @@
-const { Queue } = require('bullmq');
-const Ioredis = require('ioredis');
-
-const connection = new Ioredis(process.env.REDIS_URL || 'redis://localhost:6379', {
-  maxRetriesPerRequest: null  // Fix: Disable retries for BullMQ compatibility
-});
-const importQueue = new Queue('jobImports', { connection });
-
-module.exports = { importQueue, connection };
--- schedulers/cron.js
+++
@@ -1,9 +0,0 @@
-const cron = require('cron');
-const { fetchAndQueueJobs, API_ENDPOINTS } = require('../services/fetcher');
-
-const job = new cron.CronJob('0 * * * *', async () => { // Hourly
-  console.log('Hourly job fetch started');
-  await Promise.all(API_ENDPOINTS.map(fetchAndQueueJobs)); // Parallel for speed
-}, null, true, 'UTC');
-
-module.exports = job;
